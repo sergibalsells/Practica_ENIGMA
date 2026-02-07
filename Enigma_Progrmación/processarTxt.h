ab01d30997701d78a5172105018477f598228a57
sergibalsells <sergi.balsells@estudiant.enti.cat>
2026-02-07 18:02:19 +0100

    Comentamos los archivos de rotores y procesamiento de texto

--- Enigma_Progrmación/processarTxt.h
+++ Enigma_Progrmación/processarTxt.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 
+//Funciones con strings para poder procesar todo el texto introducido. ( Con Ayuda de la IA por temas de Errores). 
 void quitarSimbolosEspeciales(std::string& mensajeParaCifrar);
 std::string quitarAcentos(std::string& mensajeParaCifrar);
 std::string convertirMayusculas(std::string& mensajeParaCifrar);
